fcd04baa1af8777d1213b0fa24f6609b60150b48
Ryan Poe <baublet@gmail.com>
2017-07-30 15:42:25 -0400

    Add change and submit handler to text input type

--- src/Container/Inputs/Text.js
+++ src/Container/Inputs/Text.js
@@ -2,28 +2,37 @@ import React, { Component } from "react";
 import { TextInput } from "react-native";
 
 export default class extends Component {
-	render() {
-		const {
-			name,
-			style,
-			placeholder,
-			value,
-			dataType,
-			validators,
-			error,
-			onChange,
-			onSubmit,
-			...additionalProps,
-		} = this.props
-		return (
+
+    onChangeHandler = (text) => {
+        this.props.onChange(this.props.name, text)
+    }
+
+    onSubmitHandler = () => {
+        this.props.onSubmit()
+    }
+
+    render() {
+        const {
+            name,
+            style,
+            placeholder,
+            value,
+            dataType,
+            validators,
+            error,
+            onChange,
+            onSubmit,
+            ...additionalProps,
+        } = this.props
+        return (
             <TextInput
-				{...additionalProps}
+                {...additionalProps}
                 name={name}
                 style={style}
                 placeholder={placeholder}
                 onSubmitEditing={onSubmit}
                 onChangeText={(text) => this.setState({text})}
             />
-		);
-	}
+        );
+    }
 }
