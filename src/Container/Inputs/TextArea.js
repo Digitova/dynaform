602fe4e5173f6f1a056a013d4a357c98ce319e46
Ryan Poe <baublet@gmail.com>
2017-07-30 15:50:20 -0400

    Add text area component type

---
+++ src/Container/Inputs/TextArea.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Text from "./Text";
+
+export default class extends Component
+{
+    render() {
+        return (
+            <Text multiline={true} {...this.props} />
+        );
+    }
+}
