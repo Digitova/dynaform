9efb36c3f80c7acbcba6c3bdb6c3a24b5e72bd26
Christopher Rathgeb <crathgeb@digitova.com>
2017-07-30 10:20:32 -0400

    Added some basic pluming for the application

---
+++ src/inputs/Text.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { TextInput } from "react-native";
+
+
+class Text extends React.Component {
+	render() {
+		return (
+			<TextInput></TextInput>
+		);
+	}
+}
+
+
+export default Text;
