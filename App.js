d78a9bcd317474644ba5fe814212eddcda9a45b6
Christopher Rathgeb <crathgeb@digitova.com>
2017-07-30 13:41:12 -0400

    adjusted the app temporary schema to add properties and be an input type object

--- App.js
+++ App.js
@@ -5,8 +5,28 @@ import Form from './src/form'
 export default class App extends React.Component {
   render() {
       const schema = {
-          inputType: 'as',
-          xxx:'asdf',
+          inputType: 'object',
+	      properties: {
+		      "firstName": {
+			      dataType: "string",
+			      placeholder: 'Please enter your first name',
+			      inputType: "text",
+			      title: 'First Name',
+			      helper: 'Your first name, kind person!',
+			      required: true
+		      },
+		      "lastName": {
+			      dataType: "string",
+			      inputType: "text",
+			      title: 'Last Name',
+			      required: true,
+		      },
+		      "phoneNumber": {
+			      dataType: "string",
+			      inputType: "text",
+			      validator: "phoneNumber",
+		      }
+	      }
       };
 
     return (
