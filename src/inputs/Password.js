2100627d767d3f54bb2e2be49b3e381a64e3e735
Ryan Poe <baublet@gmail.com>
2017-07-30 10:58:17 -0400

    Work on text/password and build out structure of fields and inputs

--- src/inputs/Password.js
+++ src/inputs/Password.js
@@ -1,14 +1,30 @@
 import React from "react";
 import Text from "./Text";
-
+import { View } from 'react-native';
 
 class Password extends React.Component {
-	render() {
-		return (
-			<Text></Text>
-		);
-	}
+    render() {
+        const {
+            name,
+            title,
+            subtitle,
+            helper,
+            styles,
+            placeholder,
+            value,
+            dataType,
+            validators,
+            error,
+            onChange,
+            onSubmit,
+        } = this.props
+        return (
+            <View>
+                <
+                <Text />
+            </View>
+        );
+    }
 }
-
 
 export default Password;
--- src/inputs/Text.js
+++ src/inputs/Text.js
@@ -1,13 +1,31 @@
 import React from "react";
 import { TextInput } from "react-native";
 
-
 class Text extends React.Component {
-	render() {
-		return (
-			<TextInput></TextInput>
-		);
-	}
+    render() {
+        const {
+            name,
+            styles,
+            placeholder,
+            value,
+            dataType,
+            validators,
+            error,
+            onChange,
+            onSubmit,
+            ...additionalProps,
+        } = this.props
+        return (
+            <TextInput
+                {...additionalProps}
+                name={name}
+                style={styles}
+                placeholder={placeholder}
+                onSubmitEditing={onSubmit}
+                onChangeText={(text) => this.setState({text})}
+            />
+        );
+    }
 }
 
 
