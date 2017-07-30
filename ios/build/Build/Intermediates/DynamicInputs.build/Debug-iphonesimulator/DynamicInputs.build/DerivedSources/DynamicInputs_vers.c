7411bf3eb939e9593acdfc8c07fdd334aa085597
Anthony Garritano <agarritano@digitova.com>
2017-07-30 11:04:25 -0400

    Remove cached/commited build files

--- ios/build/Build/Intermediates/DynamicInputs.build/Debug-iphonesimulator/DynamicInputs.build/DerivedSources/DynamicInputs_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char DynamicInputsVersionString[];
- extern const double DynamicInputsVersionNumber;
-
- const unsigned char DynamicInputsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:DynamicInputs  PROJECT:DynamicInputs-1" "\n";
- const double DynamicInputsVersionNumber __attribute__ ((used)) = (double)1.;
