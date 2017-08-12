a91b0029d13425e7e00f025f23eb27b42c3f80ed
Christopher Rathgeb <crathgeb@digitova.com>
2017-08-11 23:08:00 -0400

    adjusted FormType to support sections

--- src/Container/ElementGenerators/FormType.js
+++ src/Container/ElementGenerators/FormType.js
@@ -1,27 +1,85 @@
 import React, { Component } from "react"
-import { ScrollView, Text } from "react-native"
+import { View, ScrollView,StyleSheet, Text } from "react-native"
 import Schema from "./Schema"
 
 export default class extends Component {
     render() {
         const { schema, data, styleObject } = this.props
-        const elements = Object.keys(schema.elements)
+	    const sections = schema.sections
 
         return (
             <ScrollView>
-                {elements.map((name, index) => {
-                    return (
-                        <Schema
-                            schema={schema.elements[name]}
-                            data={data && data[name] ? data[name] : null}
-                            key={index}
-                            name={name}
-                            onChange={this.props.onChange}
-                            styleObject={styleObject}
-                        />
-                    )
+                {sections.map((section, index) => {
+
+	                const elements = Object.keys(section.elements)
+
+                    return (<View key={index} style={{marginBottom: schema.sectionSpacing}}>
+                                {this.renderHeader(section.header)}
+	                            {this.renderSubheader(section.subheader)}
+                                {elements.map((elementName, elementIndex) => {
+                                    return (
+                                        <Schema
+                                            schema={section.elements[elementName]}
+                                            data={data && data[elementName] ? data[elementName] : null}
+                                            key={elementIndex}
+                                            name={elementName}
+                                            onChange={this.props.onChange}
+                                            styleObject={styleObject}
+                                        />
+                                    )
+                                })}
+	                            {this.renderHelper(section.helper)}
+
+                    </View>)
+
                 })}
             </ScrollView>
         )
     }
+
+	renderHeader(header) {
+		if (header) {
+			return (
+                <Text style={Styles.Header}>{header}</Text>
+			)
+		}
+		return null
+	}
+	renderSubheader(subheader) {
+		if (subheader) {
+			return (
+                <Text style={Styles.Subheader}>{subheader}</Text>
+			)
+		}
+		return null
+	}
+	renderHelper(helper) {
+		if (helper) {
+			return (
+                <Text style={Styles.Helper}>{helper}</Text>
+            )
+		}
+		return null
+	}
 }
+
+
+const Styles = StyleSheet.create({
+	Header: {
+		fontSize: 14,
+		fontWeight: '700',
+		color: "#666",
+		paddingHorizontal: 10,
+		marginTop: 20
+	},
+	Subheader: {
+		fontSize: 11,
+		color: "#666",
+		paddingHorizontal: 10
+	},
+	Helper: {
+		fontSize: 12,
+		color: "#666",
+		paddingHorizontal: 10
+	}
+})
--- tests/testSchema.js
+++ tests/testSchema.js
@@ -1,135 +1,67 @@
 export const basic = {
-	title: 'PROFILE INFORMATION',
     elementType: "form",
-    elements: {
-        "firstName": {
-            dataType: "string",
-	        templateType: "inline",
-            elementType: "text",
-	        title: 'First Name',
-	        subtitle: 'What is the user\'s First Name?',
-            placeholder: 'Christopher',
-            required: true
-        },
-        "lastName": {
-            dataType: "string",
-            elementType: "text",
-	        title: 'Last Name',
-	        subtitle: 'What is the user\'s Last Name?',
-	        placeholder: 'First Name',
-            required: true,
-        },
-	    "email": {
-		    dataType: "string",
-		    elementType: "text",
-		    title: "LOGIN INFORMATION",
-		    placeholder: "E-Mail Address",
-		    required: true,
-	    },
-        "password": {
-            dataType: 'string',
-            elementType: 'password',
-	        placeholder: 'Password',
-        },
-        "isAdmin":{
-            dataType: 'boolean',
-            elementType: "switch",
-	        templateType: "inline",
-	        title: "Admin User",
-	        subtitle: "Is this user an Admin?",
-        },
-	    "isActive":{
-		    dataType: 'boolean',
-		    elementType: "switch",
-		    templateType: "inline",
-		    title: "Active User",
-		    subtitle: "Is this an active user?",
-	    },
-	    'pickles': {
-		    dataType: 'string',
-		    elementType: 'picker',
-		    title: 'Pickle Selection',
-		    items: {
-			    dill: 'Dill Pickles',
-			    egg: 'Pickled Egg',
-			    bb: 'Bread & Butter',
-		    }
-	    },
-        "subForm": {
-            elementType: "form",
-            elements: {
-                "firstName": {
-                    dataType: "string",
-                    elementType: "text",
-                    placeholder: 'Please enter your first name',
-                    title: 'First Name',
-                    subtitle:'What yo name?',
-                    helper: 'Your first name, kind person!',
-                    required: true
-                },
-                "lastName": {
-                    dataType: "string",
-                    elementType: "text",
-                    title: 'Last Name',
-                    required: true,
-                },
-                "subForm": {
-                    elementType: "form",
-                    elements: {
-                        "firstName": {
-                            dataType: "string",
-                            elementType: "text",
-                            placeholder: 'Please enter your first name',
-                            title: 'First Name',
-                            subtitle:'What yo name?',
-                            helper: 'Your first name, kind person!',
-                            required: true
-                        },
-                        "lastName": {
-                            dataType: "string",
-                            elementType: "text",
-                            title: 'Last Name',
-                            required: true,
-                        }
-                    }
-                }
-            }
-        }
-    }
+	sections: [
+		{
+			header: "PROFILE INFORMATION",
+			elements: {
+				"firstName": {
+					dataType: "string",
+					templateType: "inline",
+					elementType: "text",
+					title: 'First Name',
+					subtitle: 'What is the user\'s First Name?',
+					placeholder: 'Christopher',
+					required: true
+				},
+				"lastName": {
+					dataType: "string",
+					elementType: "text",
+					templateType: "inline",
+					title: 'Last Name',
+					subtitle: 'What is the user\'s Last Name?',
+					placeholder: 'Rathgeb',
+					required: true,
+				}
+			}
+		},
+		{
+			header: "USER SETTINGS",
+			subheader: "Section subheading",
+			elements: {
+				"isAdmin":{
+					dataType: 'boolean',
+					elementType: "switch",
+					templateType: "inline",
+					title: "Admin User",
+					subtitle: "Is this user an Admin?",
+				},
+				"isActive":{
+					dataType: 'boolean',
+					elementType: "switch",
+					templateType: "inline",
+					title: "Active User",
+					subtitle: "Is this an active user?",
+				}
+			}
+		},
+		{
+			header: "PICKLES",
+			elements: {
+				'pickles': {
+					dataType: 'string',
+					elementType: 'picker',
+					items: {
+						dill: 'Dill Pickles',
+						egg: 'Pickled Egg',
+						bb: 'Bread & Butter',
+					}
+				}
+			}
+		}
+	]
 }
 
-export const advanced = {
-    elementType: "form",
-    elements: {
-        todos: {
-            elementType: "array",
-            elements: {
-                id: {
-                    dataType: 'readOnly',
-                    value: 'testst',
-                },
-                "task": {
-                    dataType: 'string',
-                    placeholder: 'Enter your task here...',
-                    required: 'true',
-                },
-                'description': {
-                    dataType: 'string',
-                    inputType: 'textAreaInput',
-                    placeholder: 'Some description here',
-                    required: 'false',
-                },
-                'complete': {
-                    dataType: 'boolean',
-                    inputType: 'checkBox',
-                    default: false
-                }
-            }
-        },
-    },
-}
 
 export default {
-    basic,
-    advanced
+    basic
 }
